612293915780822cd9bbdc7c0e2143b3578a6cbd
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-03-30 12:57:14 +0200

    314x: Expand the game's initialization logic

--- 314x.c
+++ 314x.c
@@ -3,6 +3,26 @@
 int main(int argc, char **argv)
 {
 	printf("3.14X - A C, Console, Picross Game\n");
-	
+
+	if (argc > 1)
+	{
+		FILE *m_game = fopen(argv[1], "r");
+
+		if (m_game != NULL)
+		{
+		}
+		else
+		{
+			printf("Could not open the file!\n");
+		}
+	}
+	else
+	{
+		printf("\nUsage:\n");
+		printf("314x [filename]\n");
+		printf("\nWhere:\n");
+		printf("[filename]: File containing the game to play.\n");
+	}
+
 	return 0;
 }
